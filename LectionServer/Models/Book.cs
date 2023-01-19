12b2bd762af904c9dfe3b19e80531562a85491ea
Sergey Prilipko <sprilipko@gmail.com>
2023-01-19 21:05:07 +0300

    initial files

---
+++ LectionServer/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LectionServer.Models;
+
+public record Book
+{
+    public required Guid Id { get; init; }
+    public required Guid UserId { get; set; }
+    public required string Author { get; set; }
+    public required string Name { get; set; }
+}
